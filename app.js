50303e8db1bf8d7532b40cb21d717d1cc67d6143
Farwa Waheed <farwawaheed714@gmail.com>
2023-09-10 16:42:15 +0500

    First Committ

---
+++ app.js
@@ -0,0 +1,28 @@
+//#################################### TODO APP ######################################
+
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+    
+    let delBtn = document.createElement("button");
+    delBtn.innerText= "delete";
+    delBtn.classList.add("delete");
+
+    ul.appendChild(item);
+    item.appendChild(delBtn);
+    
+
+    input.value = "";
+});
+
+ul.addEventListener("click",function(event){
+    if (event.target.nodeName == "BUTTON"){
+        let list = event.target.parentElement;
+        list.remove();
+    }
+
+});
